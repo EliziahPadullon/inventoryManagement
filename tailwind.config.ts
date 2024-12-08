a3669fd30fefa889aebcfddc0b20a78df0fde1fb
Eliziah Padullon <eliziahpadullon7@gmail.com>
2024-12-09 00:09:15 +0800

    Install flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
+import flowbitePlugin from "flowbite/plugin";
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [flowbitePlugin]
 } satisfies Config;
